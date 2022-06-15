f2a20b4cf6a3f17b04e6fad8765a3a75dc273265
yifanfeng97 <evanfeng97@qq.com>
2022-06-15 11:58:02 +0800

    update news

--- src/components/News.vue
+++ src/components/News.vue
@@ -8,16 +8,16 @@
         <v-timeline dense class="text-h6 font-weight-regular">
           <v-timeline-item small fill-dot>
             <v-row>
-              <v-col cols="2"> May. 2022.</v-col>
-              <v-col cols="10">
+              <v-col md="2" cols="12"> May. 2022.</v-col>
+              <v-col md="10" cols="12">
                 One paper has been accpted by IEEE T-PAMI.
               </v-col>
             </v-row>
           </v-timeline-item>
           <v-timeline-item small fill-dot>
             <v-row>
-              <v-col cols="2"> Feb. 2022.</v-col>
-              <v-col cols="10">
+              <v-col md="2" cols="12"> Feb. 2022.</v-col>
+              <v-col md="10" cols="12">
                 We organize a track "<a
                   class="text-decoration-none"
                   href="https://www.moon-lab.tech/shrec22"
